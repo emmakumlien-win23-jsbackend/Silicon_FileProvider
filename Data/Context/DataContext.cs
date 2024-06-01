e0d29c31fbadd44504b3a7a130e51f04621aab57
EmmaK93 <ems.93@hotmail.com>
2024-06-01 17:08:05 +0200

    Add project files.

---
+++ Data/Context/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<FileEntity> Files { get; set; }
+}
---
+++ Silicon_FileProvider/Services/FileService.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Data.Context;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Silicon_FileProvider.Functions;
+
+namespace Silicon_FileProvider.Services;
+
+public class FileService(DataContext context, ILogger<FileService> logger, BlobServiceClient client, BlobContainerClient container)
+{
+    private readonly DataContext _context = context;
+    private readonly ILogger<FileService> _logger = logger;
+    private readonly BlobServiceClient _client = client;
+    private BlobContainerClient _container =container;
+
+    public async Task SetBlobContainerAsync(string containerName)
+    {
+        _container = _client.GetBlobContainerClient(containerName);
+        await _container.CreateIfNotExistsAsync();
+    }
+    public string SetFileName(IFormFile file)
+    {
+        var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+        return fileName;
+    }
+    public async Task<string> UploadFileAsync(IFormFile file, FileEntity fileEntity)
+    {
+
+        BlobHttpHeaders headers = new BlobHttpHeaders()
+        {
+            ContentType = file.ContentType,
+
+        };
+
+        var blobClient = _container.GetBlobClient(fileEntity.FileName);
+
+        using var stream = file.OpenReadStream();
+        await blobClient.UploadAsync(stream, headers);
+
+        return blobClient.Uri.ToString();
+
+    }
+
+    public async Task SaveToDatabaseAsync(FileEntity fileEntity)
+    {
+        _context.Files.Add(fileEntity);
+        await _context.SaveChangesAsync();
+    }
+
+}
